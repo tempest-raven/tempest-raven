b12c68f60bb93ca8c249a5db59052567755d283e
AlphaJon <jonathan-1003@hotmail.com>
2024-09-17 21:54:55 +0200

    Add slugs to ability links and move link location to title

--- src/app/ability/ability.component.ts
+++ src/app/ability/ability.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input, numberAttribute, OnInit } from '@angular/core';
+import { booleanAttribute, Component, Input, numberAttribute, OnInit } from '@angular/core';
 import { Ability } from '../ability';
 import { AbilityService } from '../services/ability.service';
 import { PercentPipe } from '@angular/common';
 import { AbilityScalePipe } from '../pipes/ability-scale.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ability',
   standalone: true,
-  imports: [PercentPipe, AbilityScalePipe],
+  imports: [PercentPipe, AbilityScalePipe, RouterLink],
   templateUrl: './ability.component.html',
   styleUrl: './ability.component.css'
 })
 export class AbilityComponent implements OnInit {
   @Input({required: true, transform: numberAttribute}) abilityId: number = 0;
+  @Input({transform: booleanAttribute}) addLink: boolean = false;
   public ability: Ability | undefined;
 
   constructor(private abilityService: AbilityService){
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AbilityListComponent } from './ability-list/ability-list.component';
 
 export const routes: Routes = [
     { path: "abilities", component: AbilityListComponent },
+    { path: "ability/:abilityId/:slug", component: AbilityComponent },
     { path: "ability/:abilityId", component: AbilityComponent },
     { path: "", redirectTo: "abilities", pathMatch: "full"}
 ];
