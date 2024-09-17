b437214cc5010e15bfe0e3cbc72e3d03d7b53484
AlphaJon <jonathan-1003@hotmail.com>
2024-09-17 04:48:05 +0200

    Add clickable links to individual abilities

--- src/app/ability-list/ability-list.component.ts
+++ src/app/ability-list/ability-list.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { AbilityService } from '../services/ability.service';
 import { Ability } from '../ability';
 import { AbilityComponent } from "../ability/ability.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ability-list',
   standalone: true,
-  imports: [AbilityComponent],
+  imports: [AbilityComponent, RouterLink],
   templateUrl: './ability-list.component.html',
   styleUrl: './ability-list.component.css'
 })
