e40ecd9f8379dce003a471dfff1d2b19abb864de
AlphaJon <jonathan-1003@hotmail.com>
2024-09-27 01:04:14 +0200

    Add titles to all routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { BuffListComponent } from './buffs/buff-list/buff-list.component';
 
 export const routes: Routes = [
     { path: "abilities", component: AbilityListComponent, title: "Abilities", data: { navigation: true } },
-    { path: "ability/:abilityId/:slug", component: AbilityComponent },
-    { path: "ability/:abilityId", component: AbilityComponent },
+    { path: "ability/:abilityId/:slug", component: AbilityComponent, title: s => s.params["slug"] },
+    { path: "ability/:abilityId", component: AbilityComponent, title: s => "Ability ID " + s.params["abilityId"] },
     { path: "buffs", component: BuffListComponent, title: "Buffs", data: { navigation: true } },
-    { path: "buff/:buffId/:slug", component: BuffComponent },
-    { path: "buff/:buffId", component: BuffComponent },
+    { path: "buff/:buffId/:slug", component: BuffComponent, title: s => s.params["slug"] },
+    { path: "buff/:buffId", component: BuffComponent, title: s => "Buff ID " + s.params["buffId"] },
     { path: "", redirectTo: "abilities", pathMatch: "full"}
 ];
