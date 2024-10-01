9f463725525f8b5445030ed035f9cbee65717188
AlphaJon <jonathan-1003@hotmail.com>
2024-10-01 04:16:45 +0200

    Implement stat weights

--- src/app/items/item.ts
+++ src/app/items/item.ts
@@ -40,35 +40,89 @@ export class Item {
     public equipSlot: equipSlot,
     public rarity: "Common" | "Uncommon" | "Rare",
     public classRequirement: number,
-    public levelRequirement: number,
-    public sellPriceMultiplier: number
+    public level: number,
+    public valueMultiplier: number
   ){
     
   }
-
-  itemName(language: sonnyLanguage = "ENGLISH"): string | undefined {
+  
+  localizedName(language: sonnyLanguage = "ENGLISH"): string | undefined {
     return Item.langStrings[language]["ITEMNAME"]![this.id];
   }
+  
+  private static getStat(s_ratio: number, s_level: number, s_mode: boolean){
+    let s_a = 0;
+    let s_r = 0;
+    if(s_level <= 0){
+      return 0;
+    }
+    if(s_level <= 5){
+      s_a = (9.25 * s_level + 45.75) / 4 * (s_ratio / 10);
+    }
+    else{
+      s_a = (-0.0000213 * Math.pow(s_level,5) + 0.002 * Math.pow(s_level,4) - 0.0693 * Math.pow(s_level,3) + 1.43 * Math.pow(s_level,2) - 8.8533 * s_level + 39.5) * (s_ratio / 10);
+    }
+    if(s_level < 20){
+      s_r = -0.01929824 * s_level + 0.71596;
+    }
+    else{
+      s_r = 0.33;
+    }
+    if(s_mode == true){
+      s_a *= s_r / 2;
+    }
+    if(s_mode == false){
+      s_a *= 1 - s_r;
+    }
+    return s_a;
+  }
 
+  //Math.ceil(getStat(40,e + 3,false) * secondRatio[b] * (_root["KRINITEM" + itemKrinIDnow].statUpdater2[a_i] / 100))
+  get vitality(){
+    return Math.ceil(Item.getStat(40, this.level + 3, false) * this.weight() * this.rawStats.HealthUP / 100);
+  }
+  get strength(){
+    return Math.ceil(Item.getStat(40, this.level + 3, false) * this.weight() * this.rawStats.StrengthUP / 100);
+  }
+  get magic(){
+    return Math.ceil(Item.getStat(40, this.level + 3, false) * this.weight() * this.rawStats.MagicUP / 100);
+  }
+  get speed(){
+    return Math.ceil(Item.getStat(40, this.level + 3, false) * this.weight() * this.rawStats.SpeedUP / 100);
+  }
+  get focus(){
+    return this.rawStats.FocusUP;
+  }
+  get piercing(){
+    return this.rawStats.PIU.map(per => Math.round((100 + 15 * (this.level + 3)) * this.weight() * (per / 70)));
+  }
+  get defense(){
+    return this.rawStats.DIU.map(def => Math.round((100 + 15 * (this.level + 3)) * this.weight() * (def / 70)));
+  }
+
+  weight(){
+    return Item.slotWeights[this.equipSlot];
+  }
+  
   private className(language: sonnyLanguage = "ENGLISH"){
     if (this.classRequirement === 0){
       return "";
     }
     return Item.langStrings[language]["CLASS"]![this.classRequirement - 1];
   }
-
+  
   private slotName(language: sonnyLanguage = "ENGLISH"){
     if (this.equipSlot < 2){
       return "";
     }
     return Item.langStrings[language]["ITEMSS"]![this.equipSlot - 2];
   }
-
+  
   fullLevelRequirement(language: sonnyLanguage = "ENGLISH"){
     let lang = Item.langStrings[language];
-    return `${lang["MENU"]![0]}${this.levelRequirement} ${this.className(language)} ${this.slotName(language)}`;
+    return `${lang["MENU"]![0]}${this.level} ${this.className(language)} ${this.slotName(language)}`;
   }
-
+  
   flavorText(language: sonnyLanguage = "ENGLISH"){
     let text = Item.langStrings[language]["ITEMSAY"]![this.id] as string | 0;
     if (text === 0){
@@ -76,21 +130,21 @@ export class Item {
     }
     return text;
   }
-
+  
   respecValue(level: number){
     return 10 * Math.pow(level,1.5);
   }
-
+  
   buyPrice(){
     if(this.equipSlot < 2){
-        return Math.round(this.sellPriceMultiplier * this.respecValue(this.levelRequirement) * 3);
+      return Math.round(this.valueMultiplier * this.respecValue(this.level) * 3);
     } else {
-        return Math.round(this.sellPriceMultiplier * (Item.slotWeights[this.equipSlot] * 2.5) * this.respecValue(this.levelRequirement) * 3);
+      return Math.round(this.valueMultiplier * (this.weight() * 2.5) * this.respecValue(this.level) * 3);
     }
   }
-
+  
   sellPrice(){
     return Math.ceil(this.buyPrice() / 6.65);
   }
-
+  
 }
