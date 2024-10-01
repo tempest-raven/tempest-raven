5c9f02edcd7d90a2bc3212ca6eda7eaa4018e4b1
AlphaJon <jonathan-1003@hotmail.com>
2024-10-01 04:51:25 +0200

    Display item piercing and defense

--- src/app/items/item.ts
+++ src/app/items/item.ts
@@ -4,7 +4,7 @@ type equipSlot = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 export class Item {
   static langStrings: languageObject;
-  static elementArray = [
+  elementArray = [
     "Physical",
     "Magic",
     "Ice",
